d7092f76f718dc3509380b9501765e39b28b51be
yountaeseop <jangmj6931@gmail.com>
2022-07-15 00:47:04 +0900

    게시물 상세페이지 구현중

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -51,6 +51,28 @@ public class Main {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 
+			} else if (cmd.startsWith("article detail ")) {
+
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]); // "2" -> 2
+
+				boolean foundArticle = false;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = true;
+						System.out.println("이거 있던데?");
+					}
+				}
+
+				if (foundArticle == false) {
+					System.out.printf("%d번 게시물은 없어\n", id);
+					continue;
+				}
+
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
