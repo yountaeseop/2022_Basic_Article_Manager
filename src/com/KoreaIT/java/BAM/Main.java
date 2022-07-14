c800049f36ccf2bdea508153a648a93db0848460
yountaeseop <jangmj6931@gmail.com>
2022-07-15 00:39:42 +0900

    명령어 입력

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -8,9 +8,14 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		String command = sc.nextLine();
+		while (true) {
+			System.out.printf("명령어 ) ");
+			String cmd = sc.nextLine();
 
-		System.out.printf("입력된 명령어 : %s\n", command);
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
 
 		System.out.println("==프로그램 끝==");
 		sc.close();
