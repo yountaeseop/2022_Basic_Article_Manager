6dbc11cb9f050d3415836654744164a33057e1fb
yountaeseop <jangmj6931@gmail.com>
2022-07-15 00:48:52 +0900

    게시물 상세페이지 구현, 날짜(x)

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -55,22 +55,27 @@ public class Main {
 
 				String[] cmdBits = cmd.split(" ");
 
-				int id = Integer.parseInt(cmdBits[2]); // "2" -> 2
+				int id = Integer.parseInt(cmdBits[2]);
 
-				boolean foundArticle = false;
+				Article foundArticle = null;
 
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 
 					if (article.id == id) {
-						foundArticle = true;
-						System.out.println("이거 있던데?");
+						foundArticle = article;
+						break;
 					}
 				}
 
-				if (foundArticle == false) {
+				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 없어\n", id);
 					continue;
+				} else {
+					System.out.printf("번호 : %d\n", foundArticle.id);
+					System.out.printf("날짜 : 2022-12-12 12:12:12\n");
+					System.out.printf("제목 : %s\n", foundArticle.title);
+					System.out.printf("내용 : %s\n", foundArticle.body);
 				}
 
 			} else {
