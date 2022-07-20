51c6684f386c60caa42128a77c9e299205aa42cb
yountaeseop <jangmj6931@gmail.com>
2022-07-20 16:46:33 +0900

    회원가입 기능 구현 중복체크 해결 member join

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -158,17 +158,32 @@ public class App {
 					
 				} else if (cmd.equals("member join")) {
 					int id = members.size() + 1;
-					
+					String regDate = Myutill.getDate("yyyy-MM-dd HH:mm:ss");
 					System.out.printf("이름 : ");
 					String memberName = sc.nextLine();
-					System.out.printf("아이디 : ");
-					String memberId = sc.nextLine();
-					System.out.printf("비밀번호 : ");
-					String memberPw = sc.nextLine();
+					
+					String memberId = null;
 					
 					while(true) {
+						System.out.printf("아이디 : ");
+						memberId = sc.nextLine();
+						
+						if(isJoinableLoginId(memberId) == false) {
+							System.out.printf("%s은()는 이미 사용중인 아이디입니다\n", memberId);
+							continue;
+						} else {
+							break;
+						}
+					}
+					
+					String memberPw = null;   // member인스턴스에서 받을 수 있도록 미리 지정해서
+					String memberPwCheck = null; // null을 넣어준다.
+					
+					while(true) {
+						System.out.printf("비밀번호 : ");
+						memberPw = sc.nextLine();
 						System.out.printf("비밀번호 확인 : ");
-						String memberPwCheck = sc.nextLine();
+						memberPwCheck = sc.nextLine();
 						
 						if (memberPw.equals(memberPwCheck)) {
 							break;
@@ -178,7 +193,7 @@ public class App {
 					}
 					
 					
-					Member member = new Member(id, Myutill.getDate("yyyy-MM-dd HH:mm:ss"), memberId, memberPw, memberName);
+					Member member = new Member(id, regDate, memberId, memberPw, memberName);
 					members.add(member);
 
 					System.out.printf("%s님 환영합니다!!!\n", member.name);
@@ -191,6 +206,31 @@ public class App {
 
 			System.out.println("==프로그램 끝==");
 			sc.close();
+		}
+
+		private boolean isJoinableLoginId(String memberId) {
+			
+			int index = getMemberIndexBymemberId(memberId);
+			
+			if(index == -1) {
+				return true;
+			}
+			
+		return false;
+	}
+
+		private int getMemberIndexBymemberId(String memberId) {
+			
+			int i = 0;
+			
+			for(Member member : members) {
+				if(member.loginId.equals(memberId)) {
+					return i;
+				}
+				i++;
+			}
+			
+			return -1;
 		}
 
 		private int getArticleIndexByid(int id) {
